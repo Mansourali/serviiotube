8384c200581d6e7a47933d446df3d7dae7f4559c
Rolf Wessels <rolf.wessels@gmail.com>
2012-08-20 15:25:10 +0200

    Add some timeout and color to connection test

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,4 +1,7 @@
 var options = {};
+
+var errorColor = "#f37979";
+var successColor = "#79f37f";
 window.addEvent("domready", function () {
     new FancySettings.initWithManifest(function (settings) {
         options = settings;
@@ -8,22 +11,26 @@ window.addEvent("domready", function () {
             var myRequest = new Request.JSON({
                 url: connection.urlPing,
                 method: 'get',
+                timeout : 10000,
                 onRequest: function () {
                     showStatus(i18n.get("Connecting to " + connection.urlPing + " ..."))
                 },
                 onSuccess: function (response) {
                     if (response == null) {
-                        showStatus(i18n.get("Page does not contain json" + errorcode + " ..."))
+                        showStatus(i18n.get("Page does not contain json" + errorcode + " ..."), errorColor);
                     }
                     else if (response.errorCode == 0) {
-                        showStatus(i18n.get("Successfully connected to  " + connection.url + " "))
+                        showStatus(i18n.get("Successfully connected to  " + connection.url + " "), successColor);
                     }
                     else {
-                        showStatus(i18n.get("Connected but recieved error code [" + response.errorCode + "]"))
+                        showStatus(i18n.get("Connected but recieved error code [" + response.errorCode + "]"), errorColor);
                     }
                 },
                 onFailure: function (e) {
-                    showStatus(i18n.get("Failed to connect to " + connection.urlPing + " ..."))
+                    showStatus(i18n.get("Failed to connect to " + connection.urlPing + " ..."), errorColor);
+                },
+                onTimeout: function (e) {
+                    showStatus(i18n.get("Timeout to connect to " + connection.urlPing + " ..."), errorColor);
                 }
             }).get();
 
@@ -32,11 +39,13 @@ window.addEvent("domready", function () {
 
 });
 
-function showStatus(message) {
+function showStatus(message,color) {
     var elementToUpdate = $$(options.manifest.connectionStatus.container);
     if (elementToUpdate) {
         console.log(message);
-        elementToUpdate.set('html', message)
+        console.log(elementToUpdate);
+        elementToUpdate.set('html', message);
+        elementToUpdate.set('style', "margin:10px 10px 10px 0px; width:320px; padding:10px;border:1px solid #cccccc; display:block; background-color:" + color);
     }
 }
 
